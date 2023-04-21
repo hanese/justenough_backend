c83025fed66052df6bda53514c2c2b87c7b79508
Hannes Schöller <inf21132@lehre.dhbw-stuttgart.de>
2023-04-21 09:10:04 +0200

    added small login

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-from fastapi import FastAPI
+from datetime import timedelta
+from typing import Annotated
+
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+
+
+import connection_manager
 
 app = FastAPI()
 
@@ -11,3 +18,35 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+@app.post("/token", response_model=connection_manager.Token)
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+):
+    user = connection_manager.authenticate_user(connection_manager.fake_users_db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=connection_manager.ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = connection_manager.create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@app.get("/users/me/", response_model=connection_manager.User)
+async def read_users_me(
+    current_user: Annotated[connection_manager.User, Depends(connection_manager.get_current_active_user)]
+):
+    return current_user
+
+
+@app.get("/users/me/items/")
+async def read_own_items(
+    current_user: Annotated[connection_manager.User, Depends(connection_manager.get_current_active_user)]
+):
+    return [{"item_id": "Foo", "owner": current_user.username}]
