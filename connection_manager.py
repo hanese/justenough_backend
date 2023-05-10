dec24a166302564e90a08996217a08a2782a8352
hanese <hannes.schoeller@t-online.de>
2023-05-10 11:53:32 +0200

    refactored secret key

--- connection_manager.py
+++ connection_manager.py
@@ -1,3 +1,4 @@
+import os
 from datetime import datetime, timedelta
 from typing import Annotated
 import re
@@ -13,7 +14,7 @@ import psycopg2.errors
 
 # to get a string like this run:
 # openssl rand -hex 32
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+SECRET_KEY = os.getenv("hash_secret_key")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
